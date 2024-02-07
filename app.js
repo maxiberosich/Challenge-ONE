3b5c7261906a5ac7436091ab8816769ea53169ee
MaxiBerosich <mberosich@gmail.com>
2024-02-06 23:53:11 -0300

    ADD-Funcion desencriptar boton ok

--- app.js
+++ app.js
@@ -42,11 +42,60 @@ function encriptar() {
     //Muestro el texto encriptado en donde estaba la imagen inicialmente
     document.getElementById("resultado").style.display = "inline-block";
     document.getElementById("texto-resultado").value = nuevoTexto;
+
+    //Reinicio la variable nuevoTexto para que no se guarden los anteriores
     nuevoTexto = "";
 
 }
 
 function desencriptar() {
 
+    let txt = texto.value
+
+    //Recorrer el texto
+    for (let index = 0; index < txt.length; index++) {
+
+        //Guarda lo que contiene el texto en el indice en que se encuentre
+        let letra = txt.charAt(index);
+        
+        //De acuerdo a la letra que sea se suma el indice para que salte las letras que siguen y estan de sobra
+        switch (letra) {
+            case "a":
+                index++;
+                break;
+            case "e":
+                index += 4;
+                break;
+            case "i":
+                index += 3;
+                break;
+            case "o":
+                index += 3;
+                break;
+            case "u":
+                index += 3;
+                break;
+
+            default:                
+                break;
+        }
+        nuevoTexto += letra;
+    }
+
+    //Deja de mostrar la imagen de busqueda
+    document.getElementById("busqueda").style.display = "none";
+
+    //Muestro el texto encriptado en donde estaba la imagen inicialmente
+    document.getElementById("resultado").style.display = "inline-block";
+    document.getElementById("texto-resultado").value = nuevoTexto;
+
+    //Reinicio la variable nuevoTexto para que no se guarden los anteriores
+    nuevoTexto = "";
+
+}
+
+function copiar() {
+
+       
 
 }
